097bd9f92c9d558108f51dee7ec80648d8ac3c64
Mariela1412 <segovimariela76@gmail.com>
2024-09-27 12:19:32 -0600

    practica 6

---
+++ app4.js
@@ -0,0 +1,8 @@
+const nombres = ["Ana","Luis","Maria","Juan","Carlos","Elena","Sofia","Pedro"];
+
+console.log("Lista de Nombres:");
+for (let i=0; i < nombres.length; i++){
+    console.log(nombres[i]);
+}
+
+let cantidadNombres = nombres.length;
